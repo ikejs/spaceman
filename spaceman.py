110128f8d5cbf34cc0416767fbd585a1682721cd
Ike <ikeholzmann@gmail.com>
2019-09-05 21:51:59 -0700

    Create README, check if word guessed, get guessed word

--- spaceman.py
+++ spaceman.py
@@ -1,48 +1,29 @@
 import random
 
-def load_word():
-    '''
-    A function that reads a text file of words and randomly selects one to use as the secret word
-        from the list.
+letters_guessed = ['e']
 
-    Returns:
-           string: The secret word to be used in the spaceman guessing game
-    '''
+def load_word():
     f = open('words.txt', 'r')
     words_list = f.readlines()
     f.close()
-
     words_list = words_list[0].split(' ')
     secret_word = random.choice(words_list)
     return secret_word
 
 def is_word_guessed(secret_word, letters_guessed):
-    '''
-    A function that checks if all the letters of the secret word have been guessed.
+    # Loop through the letters in the secret_word and check if a letter is not in letters_guessed
+        # https://www.programiz.com/python-programming/methods/set/issuperset
+    return set(letters_guessed).issuperset(set(secret_word))
 
-    Args:
-        secret_word (string): the random word the user is trying to guess.
-        letters_guessed (list of strings): list of letters that have been guessed so far.
-
-    Returns:
-        bool: True only if all the letters of secret_word are in letters_guessed, False otherwise
-    '''
-    # TODO: Loop through the letters in the secret_word and check if a letter is not in lettersGuessed
     pass
 
 def get_guessed_word(secret_word, letters_guessed):
-    '''
-    A function that is used to get a string showing the letters guessed so far in the secret word and underscores for letters that have not been guessed yet.
-
-    Args:
-        secret_word (string): the random word the user is trying to guess.
-        letters_guessed (list of strings): list of letters that have been guessed so far.
-
-    Returns:
-        string: letters and underscores.  For letters in the word that the user has guessed correctly, the string should contain the letter at the correct position.  For letters in the word that the user has not yet guessed, shown an _ (underscore) instead.
-    '''
-
-    #TODO: Loop through the letters in secret word and build a string that shows the letters that have been guessed correctly so far that are saved in letters_guessed and underscores for the letters that have not been guessed yet
+    display_word = []
+    for letter in secret_word:
+        if letter in letters_guessed:
+            display_word.append(letter+' ')
+        else:
+            display_word.append('_ ')
 
     pass
 
@@ -92,5 +73,4 @@ def spaceman(secret_word):
 
 
 #These function calls that will start the game
-secret_word = load_word()
 spaceman(load_word())
